4b50c449cb180288867b1229bbde67468b72ee15
Hyde46 <denis.heid@protonmail.com>
2021-09-01 23:38:49 +0200

    🚀 RELEASE: v0.1.2-beta

--- Formula/hoard.rb
+++ Formula/hoard.rb
@@ -4,9 +4,9 @@
 class Hoard < Formula
     desc "Cross-platform command organizer written in Rust"
     homepage "https://github.com/Hyde46/hoard"
-    url "https://github.com/Hyde46/hoard/releases/download/v0.1.1-beta/hoard_v0.1.1-beta_x86_64-unknown-linux-musl.zip"
-    sha256 "a027d14bae06bf9f4f6c2c46027763a2c092ce7b8af5bab493a3500d312fa26a"
-    version "0.1.1-beta"
+    url "https://github.com/Hyde46/hoard/releases/download/v0.1.2-beta/hoard_v0.1.2-beta_x86_64-unknown-linux-musl.zip"
+    sha256 "5f1f28c9e02eec810a8d2930e4850dd72e24becfb2c781831cf0a67d99b149b5"
+    version "0.1.2-beta"
   
     def install
       bin.install "hoard"
