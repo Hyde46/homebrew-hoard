ed0851dda136f27d8697585bebd2c7858a2686e4
Denis <denis.heid@protonmail.com>
2022-12-21 14:40:47 +0100

    Bump to 1.3

--- Formula/hoard.rb
+++ Formula/hoard.rb
@@ -4,9 +4,9 @@
 class Hoard < Formula
     desc "Cross-platform command organizer written in Rust"
     homepage "https://github.com/Hyde46/hoard"
-    url "https://github.com/Hyde46/hoard/releases/download/v1.1.1/hoard_v1.1.1_x86_64-apple-darwin.zip"
-    sha256 "7760ef22d76ad28ca69a1242c351826f8f93104b16bfd348b79ea41ba681c11f"
-    version "1.1.1"
+    url "https://github.com/Hyde46/hoard/releases/download/v1.3.0/hoard_v1.3.0_x86_64-apple-darwin.zip"
+    sha256 "51715eb99581a2b198634a8c675440716b838369e54084fa3ff1605e5443e15e"
+    version "1.3.0"
   
     def install
       bin.install "hoard"
